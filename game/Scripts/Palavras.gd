4104cf8671b90031804065647c934bdfe74be5f4
Rafael Gomes Pinheiro <61011140+RafaelGomes00@users.noreply.github.com>
2021-04-08 14:29:47 -0300

    Debug e build
    Resolvido alguns problemas apontados na reunião de quarta feira, junto com a 4ª build, que ficou faltando ser upada.

--- game/Scripts/Palavras.gd
+++ game/Scripts/Palavras.gd
@@ -12,7 +12,7 @@ func _on_Palavra_pressed():
 	set_process(true)
 
 func _process(delta):
-	if  get_parent().get_parent().get_node("Bolsa").get_rect().position.distance_to(self.position) > 50:
+	if  get_parent().get_node("Bolsa").get_rect().position.distance_to(self.position) > 50:
 		move_and_slide(movimento * velocidade)
 	else:
 		$"/root/Global".adicionarItemBosla(palavra)
