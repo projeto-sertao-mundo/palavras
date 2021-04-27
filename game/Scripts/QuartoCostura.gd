756ffd14cc1111b256c26924fbd0883e28adf892
Rafael Gomes Pinheiro <61011140+RafaelGomes00@users.noreply.github.com>
2021-04-27 15:58:45 -0300

    Arte integrada

--- game/Scripts/QuartoCostura.gd
+++ game/Scripts/QuartoCostura.gd
@@ -14,7 +14,7 @@ func _ready():
 func _process(delta):
 	
 	if ($"/root/Global".podeSetar && !Input.is_action_pressed("Click") && palavra.name.length() > 1 && !palavra.isLetraAcentuada):
-		if numeroCaracteres <= 10:
+		if numeroCaracteres <= 15:
 			MudarLabelPreview(palavra.get_node("Label").text)
 			$"/root/Global".podeSetar = false
 		palavra.free()
@@ -42,18 +42,18 @@ func _on_Voltar2_pressed():
 func MudarCor(var r, g, b):
 	get_node("CosturaPopUp/Preview").modulate = Color(r,g,b)
 
-func _on_Rosa_pressed():
+func _on_None_pressed():
 	MudarCor(0.75, 0.14, 0.57)
-func _on_Laranja_pressed():
-	MudarCor(0.9, 0.59, 0.06)
+func _on_Roxo_pressed():
+	MudarCor(0.513726, 0.45098, 0.552941)
 func _on_Amarelo_pressed():
-	MudarCor(0.85, 1, 0)
+	MudarCor(0.811765, 0.588235, 0.062745)
 func _on_Azul_pressed():
-	MudarCor(0, 0, 1)
+	MudarCor(0.294118, 0.439216, 0.580392)
 func _on_Vermelho_pressed():
-	MudarCor(1, 0, 0)
+	MudarCor(0.635294, 0.152941, 0.07451)
 func _on_Verde_pressed():
-	MudarCor(0,1,0)
+	MudarCor(0.32549, 0.580392, 0.337255)
 
 func _on_Recomecar_pressed():
 	MudarCor(1,1,1)
@@ -70,3 +70,12 @@ func _on_Area2D_area_exited(area):
 	set_process(false)
 	$"/root/Global".podeSetar = false
 	palavra = null
+
+func _on_Quadrado_pressed():
+	self.get_node("CosturaPopUp/Preview").texture = self.get_node("CosturaPopUp/Quadrado").texture_normal
+
+func _on_Retangulo_pressed():
+	self.get_node("CosturaPopUp/Preview").texture = self.get_node("CosturaPopUp/Retangulo").texture_normal
+
+func _on_Losango_pressed():
+	self.get_node("CosturaPopUp/Preview").texture = self.get_node("CosturaPopUp/Losango").texture_normal
